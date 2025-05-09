a0a24c3ab559dd460e3a287a40286b7d6bfca7e8
userbase <samjamesleathes+github@gmail.com>
2025-05-09 14:29:33 +0100

    Update Dialog.py

--- Dialog.py
+++ Dialog.py
@@ -11,4 +11,14 @@ def dialog_create(sender, message="", delay=1):
 def tutorial_scene():
     dialog_create("????", "Hello! Welcome to the computer!", 3)
     dialog_create("????", "Are you the one they sent to save us?", 4)
-    dialog_create("????", "The Anti-Virus has been non stop detecting viruses and nothing being done!", 4.5)
+    dialog_create("????", "The Anti-Virus has been non stop detecting viruses and nothing being done!", 4)
+    dialog_create("????", "We need you to take them down!", 2)
+    dialog_create("????", "Are you up to the task?", 2)
+    dialog_create("Player", "I'll do what I can", 2)
+    dialog_create("????", "Amazing, lets get to it then", 3)
+    dialog_create("????", "My name is Tut by the way", 2)
+    #Fade to Black
+
+#def fight_1():
+    #dialog_create("Tut", "Look theres one!", 2)
+    #dialog_create("Tut", "Go get it", 1)
